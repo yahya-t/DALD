437ef23ee9f99846535fd5e3cb24c4017a701a61
yahya-t <yahya.tlr99@gmail.com>
2022-07-18 12:50:07 +0100

    Created Music Player Branch

---
+++ app/src/main/java/com/example/dald/MusicPlayer/MusicPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dald.MusicPlayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dald.R
+
+class MusicPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
